9450a898202f7e94b8e99006aa4889478213393d
LiJohnson <598420668@163.com>
2017-08-03 18:57:05 +0800

    兼容renew参数 cas4.x 版本

--- index.js
+++ index.js
@@ -249,9 +249,11 @@ CASAuthentication.prototype._login = function(req, res, next) {
 
     // Set up the query parameters.
     var query = {
-        service: this.service_url + url.parse(req.url).pathname,
-        renew: this.renew
+        service: this.service_url + url.parse(req.url).pathname
     };
+
+    if( this.renew ) query.renew = this.renew;
+
 
     // Redirect to the CAS login.
     res.redirect( this.cas_url + url.format({
